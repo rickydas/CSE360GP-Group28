ab3fd2d63b46aed2e3602c3154759911185046a9
pmacias02 <90085307+pmacias02@users.noreply.github.com>
2022-03-24 19:23:18 -0700

    Update and rename Main.java to PabloMain.java

---
+++ PabloMain.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
+
+
+
+
+
+        primaryStage.setTitle("Home Page");
+        primaryStage.setScene(new Scene(root, 720, 480));
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
