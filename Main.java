a4820ffc1f8f9e74edf8a983b9538a57b0006815
rickydas <83739079+rickydas@users.noreply.github.com>
2022-03-24 19:45:12 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,31 +1,35 @@
-//Pablo's Main
+package application;
+	
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 
 public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			
+			BorderPane root = new BorderPane();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
-
-
-
-
-
-        primaryStage.setTitle("Home Page");
-        primaryStage.setScene(new Scene(root, 720, 480));
-        primaryStage.show();
-
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+			primaryStage.show();
+			
+			primaryStage.setWidth(900);
+			primaryStage.setHeight(480);
+			primaryStage.setResizable(false);
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+		
+	}
 }
